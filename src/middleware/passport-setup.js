62747f4414946045ac131ec96b1d3c69fa1730ac
ranupl <ranupl542011@gmail.com>
2023-11-22 19:37:50 +0530

    formate change

--- src/middleware/passport-setup.js
+++ src/middleware/passport-setup.js
@@ -1,41 +1,41 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const { UserDB } = require('../model/user'); // Import your UserDB model
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const { UserDB } = require("../model/user"); // Import your UserDB model
 
-    passport.use(new GoogleStrategy({
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: process.env.CALLBACK_URL,
-        passReqToCallback: true
-    }, async function (request, accessToken, refreshToken, profile, done) {
-        try {
-            let user = await UserDB.findOne({ email: profile._json.email });
-    
-            if (!user) {
-                user = new UserDB({
-                    fullname: profile._json.name,
-                    email: profile._json.email,
-                    profile: profile._json.picture,
-                });
-    
-                await user.save();
-            }
-    
-            request.session.user = {
-                _id: user._id,
-                fullname: user.fullname,
-                email: user.email,
-                profile: user.profile,
-            };
-            
-            return done(null, request.session.user);
-            
-        } catch (error) {
-            console.error(error);
-            return done(error, null);
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+      passReqToCallback: true,
+    },
+    async function (request, accessToken, refreshToken, profile, done) {
+      try {
+        let user = await UserDB.findOne({ email: profile._json.email });
+
+        if (!user) {
+          user = new UserDB({
+            fullname: profile._json.name,
+            email: profile._json.email,
+            profile: profile._json.picture,
+          });
+
+          await user.save();
         }
-    }));
 
+        request.session.user = {
+          _id: user._id,
+          fullname: user.fullname,
+          email: user.email,
+          profile: user.profile,
+        };
 
-
-
+        return done(null, request.session.user);
+      } catch (error) {
+        console.error(error);
+        return done(error, null);
+      }
+    }
+  )
+);
