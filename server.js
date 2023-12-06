2b00b992c206ed6fcf7b5f041078317a6b38345c
ranupl <ranupl542011@gmail.com>
2023-12-06 15:13:32 +0530

    env file removed

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const bodyparser = require("body-parser");
-const passport = require('passport');
+const passport = require("passport");
 const session = require("express-session");
-const { UserDB } = require('./src/model/user'); 
+const { UserDB } = require("./src/model/user");
 const routes = require("./src/router/router");
-require('./src/middleware/passport-setup');
+require("./src/middleware/passport-setup");
 require("./src/db/connection");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use("/public", express.static(`${__dirname}/uploads`));
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -18,40 +18,43 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.use(
-    session({
-      secret: "#google#secure",
-      resave: false,
-      saveUninitialized: true,
-    })
+  session({
+    secret: "#google#secure",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser((user, done) => {
-    done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser((obj, done) => {
-    done(null, obj);
+  done(null, obj);
 });
 
-app.get('/auth/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] },)
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
-app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/failed' }),
-    async (req, res) => {
-      const email = req.user.email; 
-      const user = await UserDB.findOne({email});
-      if(user){
-        res.render("dashboard", {user});
-      } 
-});
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/failed" }),
+  async (req, res) => {
+    const email = req.user.email;
+    const user = await UserDB.findOne({ email });
+    if (user) {
+      res.render("dashboard", { user });
+    }
+  }
+);
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(PORT, () => {
-    console.log(`server is running at ${PORT}`);
-})
+  console.log(`server is running at ${PORT}`);
+});
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
-const databaseURL = "mongodb://127.0.0.1:27017/webvapar";
-
 mongoose
-  .connect(databaseURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB)
   .then(() => {
     console.log("Connected to the database");
   })
